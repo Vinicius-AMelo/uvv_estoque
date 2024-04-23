5f907756719b19022d5bce1d8facd8ec606aecb8
Gustavo Batisti <gustavo.batisti@admin.uvv>
2024-04-23 11:27:08 -0300

    Pagina Login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,44 +1,58 @@
 import Image from 'next/image';
 import '../../scss/pages/login.scss';
 import Logo from '../../../public/Logo.png';
+import background from '../../../public/background.jpg';
 
 export default function Login() {
 	return (
 		<>
-			<Image
-				width="300"
-				height="300"
-				src={Logo}
-				alt="UVV logo"
-				className="img"
-			/>
-			<h1 className="header">LOGIN</h1>
-			<div className="forms">
-				<form action="onSubmit">
-					<label className="email" htmlFor="email">
-						Email
+			<h1 className="header"></h1>
+			<div className="container">
+				<div className="forms">
+					<label className="loginText" htmlFor="loginText">
+						LOGIN
 					</label>
-					<input
-						className="emailInput"
-						type="text"
-						id="email"
-						name="email"
+					<form action="onSubmit">
+						<input
+							className="emailInput"
+							type="text"
+							id="email"
+							name="email"
+							placeholder="  Email"
+						/>
+						<input
+							className="passwordInput"
+							type="text"
+							id="password"
+							name="password"
+							placeholder="  Password"
+						/>
+						<input
+							className="submit"
+							type="submit"
+							value="Sign In"
+						></input>
+						<label className="created" htmlFor="created">
+							Cadastrar Colaborador
+						</label>
+					</form>
+				</div>
+				<div className="logo">
+					<Image
+						width="683"
+						height="768"
+						src={background}
+						alt="Fundo de tela"
+						className="background"
 					/>
-					<label className="password" htmlFor="password">
-						Password
-					</label>
-					<input
-						className="passwordInput"
-						type="text"
-						id="password"
-						name="password"
+					<Image
+						width="300"
+						height="300"
+						src={Logo}
+						alt="UVV logo"
+						className="img"
 					/>
-					<input
-						className="submit"
-						type="submit"
-						value="Submit"
-					></input>
-				</form>
+				</div>
 			</div>
 		</>
 	);
