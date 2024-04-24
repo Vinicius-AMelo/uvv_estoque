2d14c5efb3bd7d39a07aa8b271c64ccffbfd411b
Gustavo Batisti <gustavo.batisti@admin.uvv>
2024-04-24 11:17:34 -0300

    Atualizações

--- src/app/components/recordsTable.jsx
+++ src/app/components/recordsTable.jsx
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query'
 import '../../scss/components/recordsTable.scss'
 import RecordItem from './recordItem'
 import axios from 'axios'
+import SearchBar from './searchBar'
 
 export default function RecordsTable() {
 	const query = useQuery({
@@ -15,22 +16,26 @@ export default function RecordsTable() {
 	})
 
 	return (
-		<table className="records-table">
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>Nome</th>
-					<th>Descrição</th>
-					<th>Registrado por</th>
-					<th>Data registro</th>
-					<th>Quantidade</th>
-				</tr>
-			</thead>
-			<tbody>
-				{query.data?.map((item, index) => {
-					return <RecordItem key={index} record={item} />
-				})}
-			</tbody>
-		</table>
+		<>
+			<SearchBar />
+
+			<table className="records-table">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Nome</th>
+						<th>Descrição</th>
+						<th>Registrado por</th>
+						<th>Data registro</th>
+						<th>Quantidade</th>
+					</tr>
+				</thead>
+				<tbody>
+					{query.data?.map((item, index) => {
+						return <RecordItem key={index} record={item} />
+					})}
+				</tbody>
+			</table>
+		</>
 	)
 }
--- src/app/components/searchBar.jsx
+++ src/app/components/searchBar.jsx
@@ -1,17 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function SearchBar() {
 	const [searchData, setSearchData] = useState({})
 	const [inputValue, setInputValue] = useState('')
 
 	function handleChange(event) {
-		console.log(event.target.value)
+		setInputValue(event.target.value)
 	}
+
+	const query = useQuery({
+		enabled: false,
+		queryKey: ['search'],
+		queryFn: async () => {
+			const response = await axios.get('http://localhost:3001/products')
+			return response.data
+		},
+	})
+
+	useEffect(() => {
+		if (inputValue != '') {
+			query.refetch()
+		}
+	}, [inputValue])
 
 	return (
 		<>
 			<div className="header">
-				<input className="search" type="search" placeholder="pesquisar" onChange={handleChange} />
+				<input
+					className="search"
+					type="search"
+					placeholder="pesquisar"
+					onChange={handleChange}
+					value={inputValue}
+				/>
 			</div>
 		</>
 	)
