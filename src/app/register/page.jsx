5e966476a74e06679311df0650744230096491f0
Vinicius Apolinario Melo <vinicius.apolinario@admin.uvv>
2024-04-25 13:06:24 -0300

    ajustes

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,27 +1,61 @@
+'use client'
+
 import Link from 'next/link'
 import LoginContainer from '../components/loginContainer'
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+import { useForm } from 'react-hook-form'
 
 export default function Register() {
+	const { register, handleSubmit } = useForm()
+
+	const mutation = useMutation({
+		mutationFn: async (data) => {
+			console.log(data)
+			const { name, email, password } = data
+			const response = await axios.post('http://localhost:3001/register', {
+				name,
+				email,
+				password,
+			})
+			return response.data
+		},
+	})
+
+	function onSubmit(data) {
+		mutation.mutate(data)
+	}
 	return (
 		<>
 			<LoginContainer>
 				<label className="loginText" htmlFor="loginText">
 					REGISTER
 				</label>
-				<form className="input" action="onSubmit">
-					<input className="nameInput" type="text" id="name" name="name" placeholder="  Name" required />
-					<input className="emailInput" type="email" id="email" name="email" placeholder="  Email" required />
+				<form className="input" onSubmit={handleSubmit(onSubmit)}>
+					<input
+						className="nameInput"
+						type="text"
+						id="name"
+						placeholder="  Name"
+						{...register('name', { required: true })}
+					/>
+					<input
+						className="emailInput"
+						type="email"
+						id="email"
+						placeholder="  Email"
+						{...register('email', { required: true })}
+					/>
 					<input
 						className="passwordInput"
 						type="password"
 						id="password"
-						name="password"
 						placeholder="  Password"
-						required
+						{...register('password', { required: true })}
 					/>
-					<Link href={'/login'}>
-						<input className="submit" type="submit" value="Sign Up"></input>
-					</Link>
+					<button className="submit" type="submit">
+						Cadastrar
+					</button>
 				</form>
 			</LoginContainer>
 		</>
