a7df727728aa38801085833181760cc9f50f9b25
Vinicius Apolinario Melo <vinicius.apolinario@admin.uvv>
2024-04-29 07:46:27 -0300

    role

--- src/app/api/login/index.js
+++ src/app/api/login/index.js
@@ -3,54 +3,71 @@ import prisma from "../db.js";
 import jwt from "jsonwebtoken"
 
 export async function register(req, res) {
-	const { name, email, password } = req.body;
-	const saltRounds = 12;
+	try {
+		const { name, email, password } = req.body;
+		const role = req.body?.role || "DEFAULT"
+		const saltRounds = 12;
 
-	bcrypt.hash(password, saltRounds, async (error, hash) => {
-		const user = await prisma.user.create({
-			data: {
-				name,
-				email,
-				password: hash
-			}
+		bcrypt.hash(password, saltRounds, async (error, hash) => {
+			const user = await prisma.user.create({
+				data: {
+					name,
+					email,
+					password: hash,
+					role
+				}
+			});
+
+			res.sendStatus(201);
 		});
-
-		res.sendStatus(201);
-	});
+	} catch (error) {
+		res.status(400).send({ message: "Falha na autenticação", error })
+	}
 }
 
 export async function login(req, res) {
-	const { email, password } = req.body;
-	const user = await prisma.user.findUnique({
-		where: {
-			email
-		},
-	});
-	console.log(user)
-	bcrypt.compare(password, user.password, (err, result) => {
-		if (err) return res.send({ message: "Ocorreu um problema", err })
-		if (!result) return res.send({ message: "Credenciais invalidas" })
+	try {
+		const { email, password } = req.body;
+		const user = await prisma.user.findUnique({
+			where: {
+				email
+			},
+		});
+		console.log(user)
+		bcrypt.compare(password, user.password, (err, result) => {
+			if (err) return res.send({ message: "Ocorreu um problema", err })
+			if (!result) return res.send({ message: "Credenciais invalidas" })
 
-		const token = jwt.sign({ id: user.id, name: user.name, email: user.email }, "uvvAuthLogin", { expiresIn: '1h' })
-		res.send({ token, user: { id: user.id, name: user.name, email: user.email } })
-	});
+			const token = jwt.sign({ id: user.id, name: user.name, email: user.email }, "uvvAuthLogin", { expiresIn: '1h' })
+			res.send({ token, user: { id: user.id, name: user.name, email: user.email } })
+		});
+	} catch (error) {
+		res.status(400).send({ message: "Falha na validação", error })
+	}
 }
 
 export async function validateToken(req, res, next) {
-	const token = req.headers.authorization
-	if (!token) return res.send({ message: "Token não enviado!" })
+	try {
+		const token = req.headers.authorization
+		if (!token) return res.send({ message: "Token não enviado!" })
 
-	jwt.verify(token, "uvvAuthLogin", (err, decoded) => {
-		if (err) return res.send({ message: "Usuário não autenticado!" })
-		req.user = decoded;
-		next()
-	})
-
+		jwt.verify(token, "uvvAuthLogin", (err, decoded) => {
+			if (err) return res.send({ message: "Usuário não autenticado!" })
+			req.user = decoded;
+			next()
+		})
+	} catch (error) {
+		res.status(400).send({ message: "Falha na validação", error })
+	}
 }
 export async function auth(req, res) {
-	const { user } = req
-	if (!user) return res.send({ valid: false })
-	return res.send({ valid: true })
+	try {
+		const { user } = req
+		if (!user) return res.send({ valid: false })
+		return res.send({ valid: true })
+	} catch (error) {
+		res.status(400).send({ message: "Falha na autenticação", error })
+	}
 }
 
 
